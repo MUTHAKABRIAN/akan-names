f30eafeaa07abd9d257225404f388ab10a5793a0
MUTHAKABRIAN <muthakabrian@gmail.com>
2020-05-03 11:54:17 +0300

    created array

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var akanMale = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
+var akanMale = ['kwasi','Kwadwo','Kwabena','Kwaku','Yaw','kofi','Kwame'];
 var akanFemale = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
 var dayOfTheWeek =["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 function results() {
